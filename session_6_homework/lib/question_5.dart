a2e1668bf16b7b0309d8b9583066e7cff8a6709c
yonko-mo <alsantel.ma@gmail.com>
2026-02-08 17:57:04 +0200

    added another solution to question_5 in session_6_homework

--- session_6_homework/lib/question_5.dart
+++ session_6_homework/lib/question_5.dart
@@ -7,22 +7,46 @@ Q5. Find Second Largest Number
 
 import 'dart:io';
 
+// void main() {
+//   List<int> numbers = []; // 60 ,53, 10 , 20  ,15 , 25
+//   for (var i = 0; i < 6; i++) {
+//     stdout.write('please enter a number: ');
+//     numbers.add(int.parse(stdin.readLineSync()!));
+//   }
+//   int largestNumber = numbers[0]; //60
+//   int secondLargestNumber = numbers[0];
+//   for (var i = 0; i < 6; i++) {
+//     if (numbers[i] > largestNumber) {
+//       secondLargestNumber = largestNumber;
+//       largestNumber = numbers[i];
+//     } else if (numbers[i] < largestNumber && numbers[i] > secondLargestNumber) {
+//       secondLargestNumber = numbers[i];
+//     }
+//   }
+//   print('The Largest Number is $largestNumber');
+//   print('The Second Largest Number is $secondLargestNumber');
+// }
+
+// simple solution from eng tharwat
 void main() {
-  List<int> numbers = [10, 20, 15, 60, 53, 25]; // 60 ,53, 10 , 20  ,15 , 25
+  List<int> numbers = []; // 60 ,53, 10 , 20  ,15 , 25
   for (var i = 0; i < 6; i++) {
-    stdout.write('please enter a number: ');
+    stdout.write('enter a number: ');
     numbers.add(int.parse(stdin.readLineSync()!));
   }
-  int largestNumber = numbers[0]; //60
-  int secondLargestNumber = numbers[0];
-  for (var i = 0; i < 6; i++) {
-    if (numbers[i] > largestNumber) {
-      secondLargestNumber = largestNumber;
-      largestNumber = numbers[i];
-    } else if (numbers[i] < largestNumber && numbers[i] > secondLargestNumber) {
-      secondLargestNumber = numbers[i];
+  int largestNumber = findLargestNumber(numbers);
+  print('the largest number is $largestNumber');
+  numbers.remove(largestNumber);
+  int secondLargestNumber = findLargestNumber(numbers);
+  print("the second largest number is $secondLargestNumber");
+}
+
+int findLargestNumber(List<int> numbers) {
+  int largest = numbers[0];
+  for (int number in numbers) {
+    if (number > largest) {
+      largest = number;
     }
   }
-  print('The Largest Number is $largestNumber');
-  print('The Second Largest Number is $secondLargestNumber');
+  return largest;
 }
