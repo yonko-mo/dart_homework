d23bf0b3771dbe4b1b1cc00222bdcdc0e32453c6
yonko-mo <alsantel.ma@gmail.com>
2026-02-17 15:56:38 +0200

    adding another solution for question_6 in session_8_homework

--- session_8_homework/lib/question_6.dart
+++ session_8_homework/lib/question_6.dart
@@ -1,4 +1,20 @@
 /*
+Q4
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+
+Examples:
+- '()' → Valid
+- '()[]{}' → Valid
+- '(]' → Invalid
+- '([)]' → Invalid
+- '{[]}' → Valid
+*/
+/*
 Q6
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 An input string is valid if:
@@ -12,8 +28,9 @@ Examples:
 - '([)]' → Invalid
 - '{[]}' → Valid
 */
+
 void main() {
-  String s = '()';
+  String s = '{[]}';
   if (isValid(s)) {
     print('valid');
   } else {
@@ -21,11 +38,29 @@ void main() {
   }
 }
 
+// my solution
+// bool isValid(String s) {
+//   while (s.contains('()') || s.contains('[]') || s.contains('{}')) {
+//     s = s.replaceAll('()', '');
+//     s = s.replaceAll('[]', '');
+//     s = s.replaceAll('{}', '');
+//   }
+//   return s.isEmpty;
+// }
+
+// eng tharwat solution
 bool isValid(String s) {
-  while (s.contains('()') || s.contains('[]') || s.contains('{}')) {
-    s = s.replaceAll('()', '');
-    s = s.replaceAll('[]', '');
-    s = s.replaceAll('{}', '');
+  Map<String, String> brackets = {')': '(', ']': '[', '}': '{'};
+  List<String> stack = [];
+  for (var char in s.split('')) {
+    if (brackets.values.contains(char)) {
+      stack.add(char);
+    } else {
+      String openingBracket = brackets[char]!;
+      if (stack.isEmpty || stack.removeLast() != openingBracket) {
+        return false;
+      }
+    }
   }
-  return s.isEmpty;
+  return stack.isEmpty;
 }
