0836d39f47a75d4aa47f0d5a7814a975cf4334b5
yonko-mo <alsantel.ma@gmail.com>
2026-02-19 01:25:29 +0200

    adding the required answer for single number leetcode problem in session_9_homework

--- session_9_homework/lib/leetcode_problem/single_number.dart
+++ session_9_homework/lib/leetcode_problem/single_number.dart
@@ -30,17 +30,30 @@ Constraints:
 Each element in the array appears twice except for one element which appears only once.
 */
 
+// the correct solution for o(n) complexity but not with constant extra space
+
+// int singleNumber(List<int> nums) {
+//   Map<int, int> count = {};
+//   for (var element in nums) {
+//     count[element] = (count[element] ?? 0) + 1;
+//   }
+//   int result = 0;
+//   for (var element in count.entries) {
+//     if (element.value == 1) {
+//       result = element.key;
+//       break;
+//     }
+//   }
+//   return result;
+// }
+
+// the correct solution for o(n) complexity and with constant extra space using bit manipulation (logic gates)
+
 int singleNumber(List<int> nums) {
-  Map<int, int> count = {};
-  for (var element in nums) {
-    count[element] = (count[element] ?? 0) + 1;
-  }
   int result = 0;
-  for (var element in count.entries) {
-    if (element.value == 1) {
-      result = element.key;
-      break;
-    }
+
+  for (var n in nums) {
+    result ^= n;
   }
   return result;
 }
