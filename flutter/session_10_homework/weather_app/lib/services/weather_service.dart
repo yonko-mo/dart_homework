a1e157eaa7763b45196fc369e9b8477c012556fb
yonko-mo <alsantel.ma@gmail.com>
2026-05-04 02:25:47 +0300

    feat: implement WeatherService to fetch current weather data

---
+++ flutter/session_10_homework/weather_app/lib/services/weather_service.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:weather_app/model/weather_model.dart';
+import 'package:dio/dio.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apiKey = '22f30be68e4248e79ed172857231410';
+  WeatherService(this.dio);
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+        '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1',
+      );
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage =
+          e.response?.data['error']['message'] ??
+          'An error occurred while fetching weather data.';
+      throw Exception(errorMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('An unexpected error occurred');
+    }
+  }
+}
